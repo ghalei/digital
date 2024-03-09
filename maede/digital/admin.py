27dd9763dbb6dd47313b2951384ea3022794fa07
admin <maede21_1990@yahoo.com>
2024-03-09 06:07:42 -0800

    product app

--- maede/digital/admin.py
+++ maede/digital/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category,Product,Productfile
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display=['title','is_enable']
+
+class ProductfileInlineAdmin(admin.StackedInline):
+    model=Productfile
+    fields=['title','file']
+    extra=0
+
+
+@admin.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+
+  list_display=['title','is_enable']
